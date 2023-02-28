3500a452f299d43ec21db7a0f56e24740e9e42b8
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2023-02-28 20:55:40 +0530

    Prob4.java

---
+++ Prob4.java
@@ -0,0 +1,18 @@
+package Assign6;
+
+import java.util.Scanner;
+
+public class Prob4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int ans=0,pw=1;
+        while(num2>0){
+            ans+=(num2%2)*pw;
+            num2/=10;
+            pw*=2;
+        }
+        System.out.println(ans*num1);
+    }
+}
